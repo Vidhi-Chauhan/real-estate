d434d02ef1f1054274e57dbd0362ae28a21726b5
Vidhi-Chauhan <chauhanvidhi03011998@gmail.com>
2024-07-26 15:39:10 +0530

    Create files and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import Profile from "./pages/Profile";
+import LogOut from "./pages/LogOut";
+import About from "./pages/About";
 
 export default function App() {
-  return <h1 className="text-red-50">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/log-in" element={<LogIn />}></Route>
+        <Route path="/log-out" element={<LogOut />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
